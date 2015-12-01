ce68fdb2e7bb5c11c66dfe5bfc5b114bc5c58d0f
addinall <addinall@addinall.net>
2015-12-01 15:58:48 +1000

    BIG structure change,  Full stack now working.

---
+++ src/app/app.ts
@@ -0,0 +1,8 @@
+import {bootstrap, Component} from 'angular2/angular2';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>epiTWEET Angular2</h1><h1>HTML5 - CSS3</h1><h1>REST/CRUD - Python/Flask'
+})
+class AppComponent { }
+bootstrap(AppComponent);
